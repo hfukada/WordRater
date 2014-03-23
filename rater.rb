5d5ce24674b37fe1c2d9c6f1d943c0ecf4694c32
Hiroshi Fukada <hiroshi@fukada.us>
2014-03-23 19:01:15 -0400

    working along

--- rater.rb
+++ rater.rb
@@ -4,55 +4,62 @@ require 'mongo'
 require 'tempfile'
 require 'fileutils'
 
-class DictRater
-	def initialize(dictionaryFile)
-		@dictionary = {}
-		@stats = "stats.txt"
-	end
+include Mongo
 
-	def loadFile()
-		dicttext = File.open("Words/en.txt")
-		dicttext.each_line do |word|
-			@dictionary[word] = { :up => 0, :down => 0}
-		end
-		if File.exist?(@stats)
-			dicttext = File.open(@stats)
-			dicttext.each_line do |line|
-				stat = line.split()
-				@dictionary[stat[0]] = { :up = stat[1], :down = stat[2] }
-			end
-		end
+class DictRater
+	def initialize()
+		host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
+		port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
+
+		puts "Connecting to #{host}:#{port}"
+		@db = MongoClient.new(host, port).db('wordRater')
+
 	end
 
 	def dumpDict()
-		temp = TempFile.new(@stats)
-		@dictionary.each do |key, values|
-			temp.puts("#{key} #{value[:up]} #{values[:down]}")
-		end
-		temp.close
-		FileUtils.mv(temp.path, @stats)
 	end
 
 	def voteUp( word )
-		@dictionary[word][:up] += 1
+		puts word
+		entry = @db.collection('words').find({'word' => cleanword(word)}).to_a
+		puts entry
+		entry[:up] += 1
+		@db.collection('words').update({"_id" => entry[:id]}, {"$set" => {:up => entry[:up]}})
 	end
 
 	def voteDown( word )
-		@dictionary[word][:down] += 1
+		entry = @db.collection('words').find({'word' => cleanword(word)})
+		puts entry
+		entry[:down] += 1
+		@db.collection('words').update({"_id" => entry[:id]}, {"$set" => {:up => entry[:up]}})
+	end
+
+	def getRandomWord()
+		words = @db.collection('words').find.to_a
+		words[rand(words.size)-1]
+	end
+
+	def cleanword( word )
+		word.gsub(/[^0-9a-z]/i, '')
 	end
 
 end
 
-def main()
-
-end
-main()
+rater = DictRater.new
 
 get '/' do
+	word = rater.getRandomWord
+	erb "<%= word =>", :locals => {:word => word["word"], :up => word["up"], :down => word["down"]}
 end
 
-post '/up/:word' do
+post '/up/:word' do |word|
+	rater.voteUp(word)
+	word = rater.getRandomWord
+	erb "<%= word =>", :locals => {:word => word["word"], :up => word["up"], :down => word["down"]}
 end
 
-post '/down/:word' do
+post '/down/:word' do |word|
+	rater.voteDown(word)
+	word = rater.getRandomWord
+	erb "<%= word =>", :locals => {:word => word["word"], :up => word["up"], :down => word["down"]}
 end
